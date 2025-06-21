d6b5ce31628c2e8979cf1bcbae5e675a7113c4d8
Shreesh <shreeshsanyal@gmail.com>
2025-06-21 13:02:46 +0530

    changes in seat.js Enum issue fixed

--- backend/models/seat.js
+++ backend/models/seat.js
@@ -1,22 +1,18 @@
 'use strict';
 import { Model } from 'sequelize';
+import { seatType } from '../utils/common/enum.js';
 
 export default (sequelize, DataTypes) => {
   class Seat extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       this.belongsTo(models.AirPlane, {
         foreignKey: 'airplaneId',
         onDelete: 'CASCADE',
         as: 'Flight Details'
       });
-      // define association here
     }
   }
+
   Seat.init({
     row: {
       type: DataTypes.INTEGER,
@@ -26,21 +22,25 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false
     },
-
     airplaneId: {
       type: DataTypes.INTEGER,
       allowNull: false,
-
     },
     seatType: {
       type: DataTypes.ENUM,
-      values: [Business, First_class, Economy, Premium_Economy],
-      defaultValue: Economy,
-      allowNull: false,
-
-    },
+      values: [
+        seatType.Business,
+        seatType.First_class,
+        seatType.Economy,
+        seatType.Premium_Economy
+      ],
+      defaultValue: seatType.Economy,
+      allowNull: false
+    }
+  }, {
     sequelize,
-    modelName: 'Seat',
+    modelName: 'Seat'
   });
+
   return Seat;
 };
